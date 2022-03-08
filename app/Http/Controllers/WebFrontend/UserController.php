066021787c9b167f8bab9977a37411e42c7d34ad
pratyusa2021 <94213641+pratyusa2021@users.noreply.github.com>
2022-03-08 11:40:22 +0530

    Registration is working

--- app/Http/Controllers/WebFrontend/UserController.php
+++ app/Http/Controllers/WebFrontend/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\WebFrontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Hash;
 use App\Student;
+use App\User;
+
 
 class UserController extends Controller
 {
@@ -27,5 +30,30 @@ class UserController extends Controller
     public function sendOTP(Request $request){
         dd($request->all());
       
+    }
+    public function registration(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'address' => 'required',
+            'pincode' => 'integer',
+            'mobile' => 'required'
+        ], [
+            'name.required' => 'Name is required',
+            'address' => 'Address is required',
+            'pincode.integer' => 'Pin Code should be a number',
+            'mobile' => 'Mobile No is required'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->email),
+            'mobile' => $request->mobile,
+            'address' => $request->address,
+            'state' => $request->state,
+            'city' => $request->city,
+            'pincode' => $request->pincode
+        ]);
     }
 }
