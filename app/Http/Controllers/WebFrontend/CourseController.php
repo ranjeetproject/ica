f21cf7382167fbafb4845a77a162b6558438affd
Ranjeet <ranjeet.brainium@gmail.com>
2022-03-09 21:33:06 +0530

    mycouse listing

--- app/Http/Controllers/WebFrontend/CourseController.php
+++ app/Http/Controllers/WebFrontend/CourseController.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers\WebFrontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Course;
+
 
 class CourseController extends Controller
 {
-    //
-
-    public function myCourses()
+   
+    public function myCourses(Request $request)
     {
-        return view('WebFrontend.myCourse');
+        $data = Course::orderBy('id', 'DESC')->paginate(8);
+        if($request->ajax()){
+            $view = view('WebFrontend.all-course',compact('data'))->render();
+            return response()->json(['html' => $view]);
+        }
+        return view('WebFrontend.myCourse',compact('data'));
     }
 
     public function courseDetail($id)
     {
-        return view('WebFrontend.courseDetails');
+        $courseDetails = Course::find($id);
+        //dd($courseDetails);
+        return view('WebFrontend.courseDetails',compact('courseDetails'));
     }
 }
