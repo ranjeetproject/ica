40f37b2c11fa201686348ef6b1ea1c163565acfe
Ranjeet <ranjeet.brainium@gmail.com>
2022-03-11 12:20:47 +0530

    model changes

--- app/Chapter.php
+++ app/Chapter.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chapter extends Model
 {
-    public function topics()
-    {
-        return $this->hasMany('App\ChapterDetail' ,'chapter');
-    }
+    
 }
