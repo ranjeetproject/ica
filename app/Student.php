6bef7221f59626bbcdbdf7b7b016a031ccf929a3
anitesh <anitesh.brainium@gmail.com>
2022-03-14 11:33:48 +0530

    change model

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Student extends Model
+class Student extends Authenticatable
 {
     //
     protected $fillable = [
