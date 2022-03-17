4b0f2f923d5244a789e50521f3896d682646fb9a
anitesh <anitesh.brainium@gmail.com>
2022-03-17 11:32:13 +0530

    change model

--- app/Http/Controllers/WebFrontend/HomePageController.php
+++ app/Http/Controllers/WebFrontend/HomePageController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers\WebFrontend;
 
+use App\Course;
 use App\Http\Controllers\Controller;
+use App\StdCourse;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
-    public function homePageDisplay()
+    public function homePageDisplay(Request $request)
     {
-        return view('WebFrontend.home');
+        $course = Course::with('user')->whereHas('user')
+            ->with('lessons')->where('entry_from', 'NEW')->orderBy('created_at', 'DESC')->get();
+        $data = [];
+        foreach ($course as $value)
+        {
+            $stdcors = StdCourse::where('course', $value->id)->count();
+            if ($stdcors > 1) {
+                $value->status = 1;
+                $value->stdCount = $stdcors;
+            } else {
+                $value->status = $stdcors;
+                $value->stdCount = $stdcors;
+            }
+        }
+        $data['courses'] = $course;
+        return view('WebFrontend.home', $data);
     }
 
-    
+
 }
