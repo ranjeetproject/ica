636faf28ec25c4e0ab9406f771eb643e071bf549
pratyusa2021 <94213641+pratyusa2021@users.noreply.github.com>
2022-04-13 19:35:43 +0530

    Exam Event issue solve

--- app/Listeners/ExamAssigned.php
+++ app/Listeners/ExamAssigned.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ExamAssign;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Auth;
 use App\Exam;
 use App\StdExam;
 
