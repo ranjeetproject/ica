656bb1b3f58c001560b13168212a7627e7005224
Anitesh Mondal <anitesh.brainium@gmail.com>
2022-04-21 05:26:47 +0000

    In server style change

--- resources/views/WebFrontend/custom-exam-start-pagination.blade.php
+++ resources/views/WebFrontend/custom-exam-start-pagination.blade.php
@@ -1,44 +1,63 @@
 @foreach ($question as $key => $val)
-    <div class="innerlist-item">
-        <span class="countopt">{{ $question->currentPage() }}</span>
-        <div class="item_details">
-            <p>{{ $val->qus }}</p>
-            <div class="item_desc">
-                <div class="item_morecontent">
-                    <div class="item_checkoutopt">
-                        @if (@$val->qus_option)
-                            @foreach ($val->qus_option as $option)
-                                <div class="checkbtn_opt">
-                                    <input type="radio" id="test1" name="radio-group" checked>
-                                    <label for="test1">{{ $option }}</label>
-                                </div>
-                            @endforeach
-                        @endif
-
-
-
+<div class="innerlist-item quizList">
+    <span class="countopt">{{ $question->currentPage() }}</span>
+    <div class="item_details">
+        <p class="detlNewFont">{{ $val->qus }}</p>
+        <div class="item_desc">
+            <div class="item_morecontent">
+                <div class="item_checkoutopt icFlex">
+                    @if (@$val->qus_option)
+                    @foreach ($val->qus_option as $option)
+                    <div class="checkbtn_opt">
+                        <input type="radio" id="test1" name="radio-group" checked>
+                        <label for="test1">{{ $option }}</label>
                     </div>
-                    <div class="actionview">
+                    @endforeach
+                    @endif
 
-                        <button type="button" class="btn ansbtn">Answered</button>
-                        <button type="button" class="btn skippedbtn">Skipped</button>
-                        <button type="button" class="btn revdbtn">Marked for Review</button>
 
-                    </div>
+
                 </div>
+                <div class="actionview">
+
+                    <button type="button" class="btn ansbtn">Answered</button>
+                    <button type="button" class="btn skippedbtn">Skipped</button>
+                    <button type="button" class="btn revdbtn">Marked for Review</button>
+
+                </div>
+            </div>
 
 
-                <!-- <div class="actionview2">
+            <!-- <div class="actionview2">
                            <button type="button" class="btn prevbtn_info">Previous</button>
                            <button type="button" class="btn sub_btn">Submit</button>
                        </div> -->
-            </div>
         </div>
     </div>
+</div>
+
+<script>
+    $(".ansbtn").click(function() {
+        $(this).addClass("active-green");
+        $(".skippedbtn").removeClass("active-yellow");
+        $(".revdbtn").removeClass("active-blue");
+    });
+    $(".skippedbtn").click(function() {
+        $(this).addClass("active-yellow");
+        $(".ansbtn").removeClass("active-green");
+        $(".revdbtn").removeClass("active-blue");
+    });
+    $(".revdbtn").click(function() {
+        $(this).addClass("active-blue");
+        $(".ansbtn").removeClass("active-green");
+        $(".skippedbtn").removeClass("active-yellow");
+    });
+
+</script>
 @endforeach
 
 {{-- <div class="def_btn_opt morebtn_info">
        <a href="#" class="def_btn prevbtn">Previous</a>
        <a href="{{route('exam-submit')}}" class="def_btn">Skip</a>
-       <a href="#" class="def_btn nextbtn">Next</a>
-   </div> --}}
+<a href="#" class="def_btn nextbtn">Next</a>
+</div> --}}
--- resources/views/WebFrontend/exam-start.blade.php
+++ resources/views/WebFrontend/exam-start.blade.php
@@ -1,59 +1,95 @@
 @extends('WebFrontend.layout.afterLoginApp')
 @section('content')
-    <section class="header">
-        <div class="header-top">
-            @include('WebFrontend.layout.afterLoginHeaderTop')
-        </div>
-        <div class="header-bottom">
-            @include('WebFrontend.layout.afterLoginNav')
+<section class="header">
+    <div class="header-top">
+        @include('WebFrontend.layout.afterLoginHeaderTop')
+    </div>
+    <div class="header-bottom">
+        @include('WebFrontend.layout.afterLoginNav')
+    </div>
+
+</section>
+<section class="exam-list-wr">
+    <div class="container">
+        <div class="innertitle modf_heading">
+            <h3 class="e-title">
+                <span class="blue-bar addbar"></span>
+                {{ $examName }}
+                <span class="blue-bar"></span>
+            </h3>
+            <div class="dateoption">
+                <div class="dateinfo"><span>Date : </span><strong>{{ date('d-m-Y') }}</strong></div>
+                <div class="timeinfo"><span class="clockimg"><img src="images/clockimg.png" alt="">
+                    </span> <strong> 01:15:35 </strong><span> Remaining</span></div>
+            </div>
         </div>
 
-    </section>
-    <section class="exam-list-wr">
-        <div class="container">
-            <div class="innertitle modf_heading">
-                <h3 class="e-title">
-                    <span class="blue-bar addbar"></span>
-                    {{ $examName }}
-                    <span class="blue-bar"></span>
-                </h3>
-                <div class="dateoption">
-                    <div class="dateinfo"><span>Date : </span><strong>{{ date('d-m-Y') }}</strong></div>
-                    <div class="timeinfo"><span class="clockimg"><img src="images/clockimg.png" alt="">
-                        </span> <strong> 01:15:35 </strong><span> Remaining</span></div>
+        <div class="inner_content_info">
+            <div class="inner_content_view">
+                <div class="inner_block" id="questionHolder">
+                </div>
+
+                <div class="def_btn_opt morebtn_info">
+                    <a class="def_btn prevbtn" href="javascript:void(0);" onclick="previousQuestion();">Previous</a>
+                    <a href="javascript:void(0);" class="def_btn" onclick="nextQuestion();">Skip</a>
+                    <a class="def_btn nextbtn" href="javascript:void(0);" onclick="nextQuestion();">Next</a>
                 </div>
             </div>
 
-            <div class="inner_content_info">
-                <div class="inner_content_view">
-                    <div class="inner_block" id="questionHolder">
-                    </div>
-
-                    <div class="def_btn_opt morebtn_info">
-                        <a class="def_btn prevbtn" href="javascript:void(0);" onclick="previousQuestion();">Previous</a>
-                        <a href="javascript:void(0);" class="def_btn" onclick="nextQuestion();">Skip</a>
-                        <a class="def_btn nextbtn" href="javascript:void(0);" onclick="nextQuestion();">Next</a>
-                    </div>
-                </div>
 
 
-
-            </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 @section('customJavascript')
-    <script>
-        var examId = '{{ $id }}';
-        let page = 1;
-        let lastPage = 1;
-        let url = "{{ action('WebFrontend\ExamController@examStart', ['id' => $id]) }}";
-        $(function() {
-            fetch_data();
-        });
+<script>
+    var examId = '{{ $id }}';
+    let page = 1;
+    let lastPage = 1;
+    let url = "{{ action('WebFrontend\ExamController@examStart', ['id' => $id]) }}";
+    $(function() {
+        fetch_data();
+    });
 
 
-        function fetch_data() {
+    function fetch_data() {
+        $.ajax({
+                url: url + '?page=' + page,
+                method: "GET",
+                beforeSend: function() {
+                    $('#allPostLoader').show();
+                }
+            })
+            .done(function(response) {
+                $('#questionHolder').html(response.html);
+                page = response.page;
+                lastPage = response.last_page;
+            })
+    }
+
+    function previousQuestion() {
+        let innetPage = (page - 2);
+        if (innetPage > 0) {
+            $.ajax({
+                    url: url + '?page=' + innetPage,
+                    method: "GET",
+                    beforeSend: function() {
+                        $('#allPostLoader').show();
+                    }
+                })
+                .done(function(response) {
+                    $('#questionHolder').html(response.html);
+                    page = response.page;
+                })
+        } else {
+            alert('No More question for previous');
+        }
+
+    }
+
+    function nextQuestion() {
+        if (lastPage >= page) {
             $.ajax({
                     url: url + '?page=' + page,
                     method: "GET",
@@ -64,46 +100,12 @@
                 .done(function(response) {
                     $('#questionHolder').html(response.html);
                     page = response.page;
-                    lastPage = response.last_page;
                 })
+        } else {
+            alert('No More question for skip or next');
         }
+    }
 
-        function previousQuestion() {
-            let innetPage = (page - 2);
-            if (innetPage > 0) {
-                $.ajax({
-                        url: url + '?page=' + innetPage,
-                        method: "GET",
-                        beforeSend: function() {
-                            $('#allPostLoader').show();
-                        }
-                    })
-                    .done(function(response) {
-                        $('#questionHolder').html(response.html);
-                        page = response.page;
-                    })
-            } else {
-                alert('No More question for previous');
-            }
+</script>
 
-        }
-
-        function nextQuestion() {
-            if (lastPage >= page) {
-                $.ajax({
-                        url: url + '?page=' + page,
-                        method: "GET",
-                        beforeSend: function() {
-                            $('#allPostLoader').show();
-                        }
-                    })
-                    .done(function(response) {
-                        $('#questionHolder').html(response.html);
-                        page = response.page;
-                    })
-            } else {
-                alert('No More question for skip or next');
-            }
-        }
-    </script>
 @endsection
