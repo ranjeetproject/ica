aac8bdd341ca4eed868ef6158c512453742d60b1
Ranjeet <ranjeetyadav879@gmail.com>
2022-04-21 11:37:27 +0530

    style changes

--- resources/views/WebFrontend/exam-start.blade.php
+++ resources/views/WebFrontend/exam-start.blade.php
@@ -7,6 +7,7 @@
         <div class="header-bottom">
             @include('WebFrontend.layout.afterLoginNav')
         </div>
+
     </section>
     <section class="exam-list-wr">
         <div class="container">
@@ -19,51 +20,52 @@
                 <div class="dateoption">
                     <div class="dateinfo"><span>Date : </span><strong> {{ date('d-m-Y') }}</strong></div>
                     <div class="timeinfo"><span class="clockimg"><img src="{{asset('css/images/clockimg.png')}}" alt="">
-                        </span> <strong id="countdown"> </strong> <span> Remaining</span></div>
+                        </span> <strong id="countdown">  </strong> <span> Remaining</span></div>
                 </div>
             </div>
-        </div>
 
+            <div class="inner_content_info">
+                <div class="inner_content_view">
+                    <div class="inner_block" id="questionHolder">
+                        <div class="load-more" style="display:none">
+                            <img src="{{ asset('css/images/Spinner-1s-43px.gif') }}" class="img-fluid" />
+                        </div>
+                    </div>
 
-        <div class="inner_content_info">
-            <div class="inner_content_view">
-                <div class="inner_block" id="questionHolder">
-                    <div class="load-more" style="display:none">
-                        <img src="{{ asset('css/images/Spinner-1s-43px.gif') }}" class="img-fluid"/>
+                    <div class="def_btn_opt morebtn_info">
+                        <a class="def_btn prevbtn" href="javascript:void(0);" onclick="previousQuestion();">Previous</a>
+                        <a href="javascript:void(0);" class="def_btn" onclick="nextQuestion();">Skip</a>
+                        <a class="def_btn nextbtn" href="javascript:void(0);" onclick="nextQuestion();">Next</a>
                     </div>
                 </div>
 
-                <div class="def_btn_opt morebtn_info">
-                    <a class="def_btn prevbtn" href="javascript:void(0);" onclick="previousQuestion();">Previous</a>
-                    <a href="javascript:void(0);" class="def_btn" onclick="nextQuestion();">Skip</a>
-                    <a class="def_btn nextbtn" href="javascript:void(0);" onclick="nextQuestion();">Next</a>
-                </div>
+
+
             </div>
         </div>
     </section>
 @endsection
-
 @section('customJavascript')
-
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         var examId = '{{ $id }}';
         let page = 1;
         let lastPage = 1;
         let url = "{{ action('WebFrontend\ExamController@examStart', ['id' => $id]) }}";
-        $(function () {
+        $(function() {
             fetch_data();
         });
 
 
         function fetch_data() {
             $.ajax({
-                url: url + '?page=' + page,
-                method: "GET",
-                beforeSend: function () {
-                    $('#allPostLoader').show();
-                }
-            })
-                .done(function (response) {
+                    url: url + '?page=' + page,
+                    method: "GET",
+                    beforeSend: function() {
+                        $('.load-more').show();
+                    }
+                })
+                .done(function(response) {
                     $('#questionHolder').html(response.html);
                     page = response.page;
                     lastPage = response.last_page;
@@ -74,85 +76,72 @@
             let innetPage = (page - 2);
             if (innetPage > 0) {
                 $.ajax({
-                    url: url + '?page=' + innetPage,
-                    method: "GET",
-                    beforeSend: function () {
-                        $('.load-more').show();
-                    }
-                })
-                    .done(function (response) {
+                        url: url + '?page=' + innetPage,
+                        method: "GET",
+                        beforeSend: function() {
+                            $('.load-more').show();
+                        }
+                    })
+                    .done(function(response) {
                         $('#questionHolder').html(response.html);
                         page = response.page;
                     })
             } else {
-                alert('No More question for previous');
-            }
-        }
-
-
-        function previousQuestion() {
-            let innetPage = (page - 2);
-            if (innetPage > 0) {
-                $.ajax({
-                    url: url + '?page=' + innetPage,
-                    method: "GET",
-                    beforeSend: function () {
-                        $('.load-more').show();
-                    }
+                Swal.fire({
+                    icon: 'info',
+                    title: 'No more question for previous!',
+                    //text: 'No more question for previous!',
                 })
-                    .done(function (response) {
-                        $('#questionHolder').html(response.html);
-                        page = response.page;
-                    })
-            } else {
-                Swal.fire('No more question for previous')
+                //Swal.fire('No more question for previous')
                 //alert('No More question for previous');
             }
+
         }
-
-
-
 
         function nextQuestion() {
             if (lastPage >= page) {
                 $.ajax({
-                    url: url + '?page=' + page,
-                    method: "GET",
-                    beforeSend: function () {
-                        $('.load-more').show();
-                    }
-                })
-                    .done(function (response) {
+                        url: url + '?page=' + page,
+                        method: "GET",
+                        beforeSend: function() {
+                            $('.load-more').show();
+                        }
+                    })
+                    .done(function(response) {
                         $('#questionHolder').html(response.html);
                         page = response.page;
                     })
             } else {
-                Swal.fire('No more question for skip or next')
+                Swal.fire({
+                    icon: 'info',
+                    title: 'No more question for skip or next!',
+                    //text: 'No more question for previous!',
+                })
+                //Swal.fire('No more question for skip or next')
                 //alert('No More question for skip or next');
             }
         }
 
-        const startingMinuites = '{{ $duration }}';
+        const startingMinuites = 10;
         let time = startingMinuites * 60;
 
         const countdownEl = document.getElementById('countdown')
         //console.log(countdownEl)
-        const interval = setInterval(updateCountdown, 1000);
+        const interval =setInterval(updateCountdown,1000);
 
-        function updateCountdown() {
-            const minute = Math.floor(time / 60);
+        function updateCountdown(){
+            const minute = Math.floor(time/60);
             let second = time % 60
 
-            countdownEl.innerText = `${(minute < 10) ? '0' + minute : minute}: ${(second < 10) ? '0' + second : second}`
-            if (time == 0) {
+            countdownEl.innerText = `${(minute<10)?'0'+ minute:minute}: ${(second<10)?'0'+ second:second}`
+            if(time==0){
                 Swal.fire('Time is over')
                 clearInterval(interval)
-            } else {
+            }else{
                 time--
             }
 
         }
-
+        
     </script>
-
 @endsection
