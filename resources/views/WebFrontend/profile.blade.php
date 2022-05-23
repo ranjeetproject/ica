2e82bfe23772c4561788d51ab94a29e5d20d7a7f
Ranjeet <ranjeetyadav879@gmail.com>
2022-05-23 17:08:15 +0530

    changes in prfile

--- resources/views/WebFrontend/profile.blade.php
+++ resources/views/WebFrontend/profile.blade.php
@@ -273,6 +273,24 @@
 
         });
     });
+    @if(Session::has('success'))
+        toastr.success("{{ Session::get('success') }}");
+        {{ Session::forget('success') }};
+    @endif
 
+
+    @if(Session::has('info'))
+        toastr.info("{{ Session::get('info') }}");
+    @endif
+
+
+    @if(Session::has('warning'))
+        toastr.warning("{{ Session::get('warning') }}");
+    @endif
+
+
+    @if(Session::has('error'))
+        toastr.error("{{ Session::get('error') }}");
+    @endif
 </script>
 @endsection
