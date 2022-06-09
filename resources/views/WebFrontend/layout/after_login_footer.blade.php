884869625f995dc8e27dd9aa56b6cc60703f630e
anitesh <anitesh.brainium@gmail.com>
2022-06-09 09:54:59 +0530

    right click off

--- resources/views/WebFrontend/layout/after_login_footer.blade.php
+++ resources/views/WebFrontend/layout/after_login_footer.blade.php
@@ -133,14 +133,13 @@
 <script src="https://unpkg.com/cropperjs"></script>
 <script src="{{ asset('js/WebFrontend/jquery.jqZoom.js') }}"></script>
 <script>
-//$("body").on("contextmenu",function(){
-//    return false;
-//});
-//$("img").on("contextmenu",function(){
- //   return false;
-//});
+$("body").on("contextmenu",function(){
+   return false;
+});
+$("img").on("contextmenu",function(){
+   return false;
+});
 </script>
 @yield('customJavascript')
 </body>
-
 </html>
