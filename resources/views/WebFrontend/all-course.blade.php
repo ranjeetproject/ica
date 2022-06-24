02708aa845d14220a42e8f7d95860976ee564ca2
Ranjeet <ranjeetyadav879@gmail.com>
2022-06-24 11:11:11 +0530

    all-course changes

--- resources/views/WebFrontend/all-course.blade.php
+++ resources/views/WebFrontend/all-course.blade.php
@@ -1,7 +1,11 @@
 @foreach ($data as $courses)
     <div class="col-lg-3 col-md-4 col-sm-6">
         <div class="course-card">
+        @if($courses->course_photo !='')
             <img src="{{$courses->course_photo}}" class="course-image" alt="#">
+        @else
+            <img src="{{asset('css/images/course-image.jpg')}}" class="course-image" alt="exam-image">
+        @endif
             <h4 class="course-name">{{$courses->course_name}} </h4>
             <a href="{{ url('course-details', $courses->course) }}" class="btn view">view</a>
         </div>
